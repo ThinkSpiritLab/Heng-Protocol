c9df4ad04b790a121c65ce8f4c2a564ed0605c5d
HuGuang@NewDesktop <1395943920@qq.com>
2020-08-05 11:49:49 +0800

    加入一些必要的文件

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-env node */
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "prettier",
+    ],
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "@typescript-eslint/no-namespace": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["warn"],
+    },
+};
